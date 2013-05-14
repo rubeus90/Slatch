e2d5de7509f4b2e7a2befbf2a1339a5faffbcc00
Modjow.Jm9@gmail.com <Modjow.Jm9@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-14 14:53:08 +0000

    Classe Slatch : comme ça c'est fait

---
+++ trunk/Slatch.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Slatch here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Slatch
+{
+    // instance variables - replace the example below with your own
+    private IHM myIHM;
+    
+    public Slatch () {
+        myIHM = new IHM(800,500,"Partie");
+    }
+}
