9c4feb8f3db227b80acf8bd04548fb5d2e006864
Modjow.Jm9@gmail.com <Modjow.Jm9@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-14 11:46:55 +0000

    Mise en place de la sourie

--- trunk/IHM.java
+++ trunk/IHM.java
@@ -3,20 +3,11 @@ import java.awt.* ;
 
 
 /**
- * JFrame du Menu du jeu. (herite de JFrame)
+ * JFrame est l'IHM du jeu. (herite de JFrame)
  * (Ne definit que le nom de la fenetre, sa taille, si elle est redimenssionable...).
  * 
- * Permet de lancer "Age of ESIEE Terminal" et "Age of ESIEE Arcade".
- * Creer une JFrame myMenu grace a la classe MenuPanneau qui detaille le
- * contenu de la fenetre Menu.
- * Creer un Clavier grace à la classe Clavier du package pkg_Clavier
- * qui permet d'utiliser le clavier.
- * 
- * Faites "clic droit" sur la classe Game puis "new Game()" pour lancer le jeu.
- * 
- * @author MERCANDALLI PRUDENT
- * @version 01/2013
- * @since   09/2012
+ * @author Jonathan
+ * @version 01
  */
 public class IHM extends JFrame {
     public static IHM_Pannel Menu1 = new IHM_Pannel();
@@ -29,12 +20,14 @@ public class IHM extends JFrame {
     public IHM(){
         myMenu = new JFrame("Menu");
         myMenu.setTitle("Menu");
-        myMenu.setSize(1000, 700);
+        myMenu.setSize(800,500);
         myMenu.setResizable(false);
         myMenu.setLocationRelativeTo(null);
         myMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
         myMenu.setContentPane(Menu1);    
         myMenu.setVisible(true);
+        Mouse lecteur = new Mouse();
+        myMenu.addMouseListener(lecteur);
     }
     
     /**
@@ -52,3 +45,4 @@ public class IHM extends JFrame {
         newMenu = new Menu();
     }
 }
+
