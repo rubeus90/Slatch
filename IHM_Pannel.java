229e039593b41a7b8d66d6ddfd9456fd6e5331f2
Modjow.Jm9 <Mercandj@users.noreply.github.com>
2013-05-14 11:47:45 +0000

    mise en place de la sourie

--- IHM_Pannel.java
+++ IHM_Pannel.java
@@ -13,19 +13,22 @@ import java.io.*;
  * @version 01/2013
  */
 class IHM_Pannel extends JPanel {
-     
+    public static void clikcount(int pcoucou) {
+        System.out.println(pcoucou);
+    }
+
     /**
      */
     @Override
     public void paintComponent(final Graphics g) {
         afficheImagePleinEcran("1.png",g);
-        afficheImage("2.png",50, 100,g);
+        afficheImageRedim("2.png",5, 5,100,100,g);
     }    
 
     /**
      * Affiche une image a la position desiree
      */
-    private void afficheImage(final String pURL, final int pPosX, final int pPosY, final Graphics g){
+    private void afficheImage(final String pURL, final int pPosX, final int pPosY, final Graphics g) {
         try {
             Image img = ImageIO.read(new File("Images/"+pURL));
             g.drawImage(img, pPosX, pPosY, this);
@@ -34,6 +37,21 @@ class IHM_Pannel extends JPanel {
             e.printStackTrace();
         }
     } 
+    
+    
+    /**
+     * Affiche une image en fond d'ecran
+     */
+    private void afficheImageRedim(final String pURL, final int pPosHautGaucheX, final int pPosHautGaucheY,final int pPosBasDroiteX, final int pPosBasDroiteY, final Graphics g) {
+        try {
+            Image img = ImageIO.read(new File("Images/"+pURL));
+            g.drawImage(img, pPosHautGaucheX, pPosHautGaucheY, pPosBasDroiteX, pPosBasDroiteY, this);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    } 
+    
     
     /**
      * Affiche une image en fond d'ecran
@@ -57,5 +75,6 @@ class IHM_Pannel extends JPanel {
          catch (InterruptedException e) {e.printStackTrace();}
     }
 }
+
 
 
