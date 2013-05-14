9ed528a320a73445217c516f1c7d8479d67acf7f
Modjow.Jm9@gmail.com <Modjow.Jm9@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-14 17:47:22 +0000

    Correction de bugs + les STATICs "remonté" à la classe Slatch.

--- trunk/Slatch.java
+++ trunk/Slatch.java
@@ -1,11 +1,14 @@
 /**
- * @author TLM
+ * @author Jonathan
+ * @version 1.0
  */
 public class Slatch
 {
-    private IHM myIHM;
-    
+    private static IHM myIHM;
     public Slatch () {
         myIHM = new IHM(800,500,"Partie");
+    }
+    public static IHM getIHM() {
+        return myIHM;
     }
 }
