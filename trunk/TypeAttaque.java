41e8f70d4da41b60343bd83f5a14e2e27c1436c6
martelandy764@gmail.com <martelandy764@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-15 07:44:27 +0000

    J'ai juste changé l'utilisation de l'efficacité

--- trunk/TypeAttaque.java
+++ trunk/TypeAttaque.java
@@ -4,7 +4,7 @@ public enum TypeAttaque
 	OMEGA_SLASH_DE_L_ULTIME_APPOLON(250, null, "Omega Slash de l'ultime Appolon", "Une série de coups frénétiques qui laisseront tous les ennemis sur le tapis"); // en attente de créer les vraies attaques
 	
 	int degats; // dégâts de base de l'attaque
-	HashMap<TypeUnite, Integer> efficacite; // pourcentage qui permettra de calculer les dégâts finaux
+	HashMap<TypeUnite, Integer> efficacite; // on multipliera l'attaque par ce nombre, donc 1 est l'élément neutre, quand c'est plus grand que 1 on a une efficacité plus grande, et réciproquement
 	String nom;
 	String description;
 	
