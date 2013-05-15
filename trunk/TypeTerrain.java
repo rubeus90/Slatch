b832615a0daa6e7da68d02631816f8c311408a4c
Modjow.Jm9@gmail.com <Modjow.Jm9@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-15 09:53:37 +0000

    Modif typeterrain (pas fini)

--- trunk/TypeTerrain.java
+++ trunk/TypeTerrain.java
@@ -1,8 +1,10 @@
+import java.util.HashMap;
+import java.lang.Integer;
 
 public enum TypeTerrain {
-	FORET(0,0,"foret", "Images/1.png", "une foret", 2, 2), 
-	PLAINE(0,0,"plaine", "Images/2.png", "une plaine", 1, 1), 
-	MONTAGNE(0,0,"montagne", "Images/3.png", "une montagne", 3, 3);
+	FORET(0,0,"foret", "1.png", "une foret", 2), 
+	PLAINE(0,0,"plaine", "2.png", "une plaine", 1), 
+	MONTAGNE(0,0,"montagne", "3.png", "une montagne", 3);
 	
 	private int aJoueur;
 	private int aPointDeVie;
@@ -10,16 +12,16 @@ public enum TypeTerrain {
 	private String aImage;
 	private String aDescription;
 	private int aCouverture;
-	private int aCoutDeplacement;
+	private HashMap<String,Integer> aCoutDeplacement;
 	
-	TypeTerrain(final int pJoueur, final int pPointDeVie, final String pNom, final String pImage, final String pDescription, final int pCouverture, final int pCoutDeplacement){
+	TypeTerrain(final int pJoueur, final int pPointDeVie, final String pNom, final String pImage, final String pDescription, final int pCouverture){
 		aJoueur = pJoueur;
 		aPointDeVie = pPointDeVie;
 		aNom = pNom;
 		aImage = pImage;
 		aDescription = pDescription;
 		aCouverture = pCouverture;
-		aCoutDeplacement = pCoutDeplacement;
+		aCoutDeplacement = new HashMap<String,Integer> ();
 	}
 	
 	public int getJoueur(){
@@ -46,7 +48,7 @@ public enum TypeTerrain {
 		return aCouverture;
 	}
 	
-	public int getCoutDeplacement(){
+	/*public int getCoutDeplacement(){
 		return aCoutDeplacement;
-	}
+	}*/
 }
