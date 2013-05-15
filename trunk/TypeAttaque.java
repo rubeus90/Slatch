7f5ff5781a95ad8887d84fad8131e3254269cec3
RollotQuentin@gmail.com <RollotQuentin@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-15 09:26:08 +0000

    TypeAttaque remplissage de la HashMap

--- trunk/TypeAttaque.java
+++ trunk/TypeAttaque.java
@@ -4,14 +4,15 @@ public enum TypeAttaque
 	OMEGA_SLASH_DE_L_ULTIME_APPOLON(250, null, "Omega Slash de l'ultime Appolon", "Une série de coups frénétiques qui laisseront tous les ennemis sur le tapis"); // en attente de créer les vraies attaques
 	
 	int degats; // dégâts de base de l'attaque
-	HashMap<TypeUnite, Integer> efficacite; // on multipliera l'attaque par ce nombre, donc 1 est l'élément neutre, quand c'est plus grand que 1 on a une efficacité plus grande, et réciproquement
+	HashMap<TypeUnite, Double> efficacite = new HashMap<TypeUnite, Double>(); // on multipliera l'attaque par ce nombre, donc 1 est l'élément neutre, quand c'est plus grand que 1 on a une efficacité plus grande, et réciproquement
 	String nom;
 	String description;
 	
-	TypeAttaque(int pDegats, HashMap<TypeUnite, Integer> pEfficacite, String pNom, String pDescription)
+	TypeAttaque(int pDegats, HashMap<TypeUnite, Double> pEfficacite, String pNom, String pDescription)
 	{
 		degats = pDegats;
-		efficacite = pEfficacite;
+		efficacite.put(TypeUnite.NAVAL, 1.5);
+		//efficacite = pEfficacite;
 		nom = pNom;
 		description = pDescription;
 	}
