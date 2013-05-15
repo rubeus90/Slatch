d9edea16238c46ce6e5166d4d2cef7fd212fdc20
Modjow.Jm9 <Mercandj@users.noreply.github.com>
2013-05-15 06:32:15 +0000

    Correction bug

--- IHM_Panel.java
+++ IHM_Panel.java
@@ -48,7 +48,7 @@ class IHM_Panel extends JPanel {
         tabString = pcoordclick.split(",");
         int clickX = Integer.parseInt(tabString[0]);
         int clickY = Integer.parseInt(tabString[1])-20;   //Decalage de 20 je sais pas pourquoi  
-        Graphics g = Slatch.getIHM().getmyPanel().getGraphics();
+        Graphics g = Slatch.getSlatch().getIHM().getmyPanel().getGraphics();
         for(int i = 0 ; i < NOMBRE_DE_CASE_X ; i++) {
             for(int j = 0 ; j < NOMBRE_DE_CASE_Y ; j++) {
                 // Selection
@@ -125,7 +125,7 @@ class IHM_Panel extends JPanel {
         try {
             Image img = ImageIO.read(new File("Images/"+pURL));
             //g.drawImage(img, pPosHautGaucheX, pPosHautGaucheY, pLargeur, pHauteur, IHM.getMenu1());
-            g.drawImage(img, pPosHautGaucheX, pPosHautGaucheY, pPosBasDroiteX-pPosHautGaucheX, pPosBasDroiteY-pPosHautGaucheY, Slatch.getIHM().getmyPanel());
+            g.drawImage(img, pPosHautGaucheX, pPosHautGaucheY, pPosBasDroiteX-pPosHautGaucheX, pPosBasDroiteY-pPosHautGaucheY, Slatch.getSlatch().getIHM().getmyPanel());
         }
         catch (IOException e) {
             e.printStackTrace();
