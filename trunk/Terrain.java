dd993cfd224d9c09da66c42f175cb0f23331e383
scalamat13@gmail.com <scalamat13@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-15 07:06:19 +0000

    une terrain a une unite

--- trunk/Terrain.java
+++ trunk/Terrain.java
@@ -14,6 +14,7 @@ import java.awt.Image;
 public class Terrain extends Entite{
     private String aType;
     private int aCouverture;
+    private Unite aUnite;
     private HashMap<String,Integer> aCoutDeplacement;
     
     /**
@@ -38,24 +39,35 @@ public class Terrain extends Entite{
     }
     
     /**Retourner le type du terrain (son nom)
-    *
     *@return aType
     */
     public String getType(){
         return aType;
     }
     
+    /**Retourner l'unite se trouvant sur le terrain
+    *@return aUnite
+    */
+    public Unite getUnite(){
+        return aUnite;
+    }
+    
     /** Retourner la couverture du terrain, c'est a dire la capacite de chaque type de terrain d'augmenter 
-    *ou de diminuer certains proprietes de l'unite qui se trouve sur ce type de terrain
-    *
-    *@return aCouverture
+    *ou de diminuer certains proprietes de l'unite qui se trouve sur ce type de terrain 
+    **@return aCouverture
     */
     public int getCouverture(){
         return aCouverture;
     }
     
+    /**Attribut une unite à un terrain
+    *@return aUnite
+    */
+    public void setUnite(final Unite pUnite){
+        aUnite = pUnite;
+    }
+    
     /**Retourner le cout de deplacement du terrain. Ce cout varie selon le type du terrain
-    *
     *@return aCoutDeplacement
     */
     public HashMap<String,Integer> getCoutDeplacement(){
