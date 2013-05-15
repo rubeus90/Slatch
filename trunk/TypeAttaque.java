94074235237c4f543a1103d983a019d80108f6ba
martelandy764@gmail.com <martelandy764@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-15 11:50:37 +0000

    orthographe corrigée

--- trunk/TypeAttaque.java
+++ trunk/TypeAttaque.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 public enum TypeAttaque
 {
-	OMEGA_SLASH_DE_L_ULTIME_APPOLON(250, null, "Omega Slash de l'ultime Appolon", "Une série de coups frénétiques qui laisseront tous les ennemis sur le tapis"); // en attente de créer les vraies attaques
+	OMEGA_SLASH_DE_L_ULTIME_APOLLON(250, null, "Omega Slash de l'ultime Apollon", "Une série de coups frénétiques qui laisseront tous les ennemis sur le tapis"); // en attente de créer les vraies attaques
 	
 	int degats; // dégâts de base de l'attaque
 	HashMap<TypeUnite, Double> efficacite = new HashMap<TypeUnite, Double>(); // on multipliera l'attaque par ce nombre, donc 1 est l'élément neutre, quand c'est plus grand que 1 on a une efficacité plus grande, et réciproquement
