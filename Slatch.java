18321c94694bbb1357535f6fdc4c3db8ecf8a541
Modjow.Jm9 <Mercandj@users.noreply.github.com>
2013-05-15 09:10:22 +0000

    Slatch créé la Partie, le moteur puis l'ihm

--- Slatch.java
+++ Slatch.java
@@ -5,10 +5,12 @@
 public class Slatch
 {
     public static IHM myIHM;
-    //public static Partie myPartie;
-    //public static Moteur myMoteur;
-    //public static Slatch mySlatch;
-    public Slatch () {
+    public static Partie myPartie;
+    public static Moteur myMoteur;
+    public Slatch () 
+    {
+        myPartie = new Partie();
+        myMoteur = new Moteur();
         myIHM = new IHM(800,500,"Partie");
     }
     
