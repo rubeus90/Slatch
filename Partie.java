2fc01a975bbb3f5bff9a06540f04c8d9d67a85db
t.patois <7h1b0@users.noreply.github.com>
2013-05-15 07:56:57 +0000

    Premiere version de Partie

---
+++ Partie.java
@@ -0,0 +1,123 @@
+
+/**
+ * Write a description of class Partie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Partie
+{
+    // instance variables - replace the example below with your own
+    private int aNbrJoueur;
+    private int aRevenuBatiment;
+    private int aLargeur;
+    private int aHauteur;
+    private int aTourMax;
+    private int aTour;
+    
+
+    /**
+     * Constructor for objects of class Partie
+     */
+    public Partie(final int pNbrJoueur,final int pRevenuBatiment,final int pTourMax,final int pLargeur, final int pHauteur)
+    {
+        aNbrJoueur = pNbrJoueur;
+        aTourMax = pTourMax;
+        aTour = 1;
+        aRevenuBatiment = pRevenuBatiment;
+        aLargeur = pLargeur;
+        aHauteur = pHauteur;
+        
+    }
+
+    
+    /*************************************************************
+     * 
+     * 
+     *  ACCESSEUR
+     * 
+     * 
+     * *************************************************************/
+     
+    /**
+     * Accesseur qui renvoi la valeur de la hauteur du plateau
+     * @return aHauteur 
+     */
+    public int getLargeur()
+    {
+        return aHauteur;
+    }
+    
+    /**
+     * Accesseur qui renvoi la valeur de la largeur du plateau
+     * @return aLargeur 
+     */
+    public int getLongueur()
+    {
+        return aLargeur;
+    }
+    
+    /**
+     * Accesseur qui renvoi la valeur le tour actuel du plateau
+     * @return aTour 
+     */
+    public int getTour()
+    {
+        return aTour;
+    }
+    
+    /**
+     * Accesseur qui renvoi la valeur du nombre de tour maximum du plateau
+     * @return aTourMax 
+     */
+    public int getTourMax()
+    {
+        return aTourMax;
+    }
+    
+    /**
+     * Accesseur qui renvoi les revenus par batiments
+     * @return aRevenuBatiment
+     */
+    public int getRevenuBatiment()
+    {
+        return aRevenuBatiment;
+    }
+    
+    /**
+     * Accesseur qui renvoi le nombre de jouer
+     * @return aNbrJoueur
+     */
+    public int getNbrJoueur()
+    {
+        return aNbrJoueur;
+    }
+    
+    /*************************************************************
+     * 
+     * 
+     *  MODIFICATEUR
+     * 
+     * 
+     * *************************************************************/
+     
+     /**
+     * Modificateur qui modifie la valeur du tour actuel
+     * @param pTour
+     */
+    public void setTour(final int pTour)
+    {
+        aTour = pTour;
+    }
+    
+    /**
+     * Modificateur qui modifie la valeur du tour actuel
+     * @param pTour
+     */
+    public void setRevenuBatiment(final int pRevenuBatiment)
+    {
+        aRevenuBatiment = pRevenuBatiment;
+    }
+         
+    
+}
