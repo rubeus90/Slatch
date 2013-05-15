fd79aa9bccb35edb6ab5085c2af33f0945c272c5
Modjow.Jm9@gmail.com <Modjow.Jm9@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-15 12:29:44 +0000

    correction IHM & Slatch

--- trunk/Slatch.java
+++ trunk/Slatch.java
@@ -4,16 +4,24 @@
  */
 public class Slatch {
 
-	public static IHM ihm;
-  public static Partie partie;
-  public static Moteur moteur;
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    public static Slatch slatch;
+    public static IHM ihm;
+    public static Partie partie;
+    public static Moteur moteur;
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        slatch = new Slatch();
+    }
+    
+    /**
+     * @param args
+     */
+    public Slatch() {
         partie = new Partie(2, 20, 30, null);
         moteur = new Moteur();
         ihm = new IHM(800,500);
-	}
+    }
 }
