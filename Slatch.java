4eb58b42e46c6120ef43f3dae5e30d73d9e31980
scalamat13 <KarlakS2@users.noreply.github.com>
2013-05-15 06:19:27 +0000

    objet slatch en statique

--- Slatch.java
+++ Slatch.java
@@ -5,8 +5,14 @@
 public class Slatch
 {
     private static IHM myIHM;
+    private static Slatch mySlatch;
     public Slatch () {
         myIHM = new IHM(800,500,"Partie");
+        setSlatch();
+    }
+    public void setSlatch()
+    {
+        mySlatch=this;
     }
     public static IHM getIHM() {
         return myIHM;
