6638d2eb78498e102b524853339e4823491def2e
t.patois@gmail.com <t.patois@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-15 11:54:48 +0000

    TypeUnit mise à jour

--- trunk/TypeUnite.java
+++ trunk/TypeUnite.java
@@ -3,5 +3,30 @@
  */
 public enum TypeUnite
 {
-	INFANTERIE, VEHICULE, AERIEN, NAVAL;
+    INFANTERIE("infanterie", "1.png","une infanterie"), 
+    VEHICULE("vehicule","1.png","un vehicule");
+
+    private String aNom;
+    private String aDescription;
+    private String aImage;
+    
+    TypeUnite(final String pNom,final String pDescription,final String pImage){
+        aNom=pNom;
+        aDescription =pDescription;
+        aImage = pImage;
+    }
+    
+    public String getNom(){
+        return aNom;
+    }
+    
+    public String getDescription(){
+        return aDescription;
+    }
+    
+    public String getImage(){
+        return aImage;
+    }
+
+
 }
