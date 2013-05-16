e23f7941310227eaaa69254ea22632c9a1456235
RollotQuentin <rollotquentin@gmail.com>
2013-05-16 09:48:14 +0000

    Amélioration de TypeAttaque.

--- TypeAttaque.java
+++ TypeAttaque.java
@@ -1,19 +1,49 @@
 import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public enum TypeAttaque
 {
-	OMEGA_SLASH_DE_L_ULTIME_APOLLON(250, null, "Omega Slash de l'ultime Apollon", "Une série de coups frénétiques qui laisseront tous les ennemis sur le tapis"); // en attente de créer les vraies attaques
-	
-	int degats; // dégâts de base de l'attaque
-	HashMap<TypeUnite, Double> efficacite = new HashMap<TypeUnite, Double>(); // on multipliera l'attaque par ce nombre, donc 1 est l'élément neutre, quand c'est plus grand que 1 on a une efficacité plus grande, et réciproquement
-	String nom;
-	String description;
-	
-	TypeAttaque(int pDegats, HashMap<TypeUnite, Double> pEfficacite, String pNom, String pDescription)
-	{
-		degats = pDegats;
-		//efficacite.put(TypeUnite.NAVAL, 1.5);
-		//efficacite = pEfficacite;
-		nom = pNom;
-		description = pDescription;
-	}
+    OMEGA_SLASH_DE_L_ULTIME_APOLLON(250, "Omega Slash de l'ultime Apollon", "Une série de coups frénétiques qui laisseront tous les ennemis sur le tapis"), // en attente de créer les vraies attaques
+    CANON(400, "Tir de canon", "Envoie un obus sur votre ennemi. Efficace contre l'infanterie."),
+    FUSIL(250, "Tir de fusil", "Envoie une salve de balles sur votre ennemi.");
+    
+    private int degats; // dégâts de base de l'attaque
+    private String nom;
+    private String description;
+    public HashMap<TypeUnite, Double> efficacite;// on multipliera l'attaque par ce nombre, donc 1 est l'élément neutre, quand c'est plus grand que 1 on a une efficacité plus grande, et réciproquement
+    
+    
+    TypeAttaque(int pDegats, String pNom, String pDescription ) // ajouter PorteeAttaque pPortee
+    {
+        degats = pDegats;
+        //efficacite.put(TypeUnite.NAVAL, 1.5);
+        //efficacite = pEfficacite;
+        nom = pNom;
+        description = pDescription;
+        efficacite= new HashMap<TypeUnite,Double> ();
+        
+        Scanner fichier = null;
+        
+        try {
+            fichier = new Scanner(new File("TypeAttaque.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        String ligne;
+        String[] tab;
+        
+        while(fichier.hasNextLine()){
+            ligne = fichier.nextLine();
+            tab = ligne.split(":");
+            if(tab[0].equals(pNom)){
+                for(TypeUnite type : TypeUnite.values()) {
+                    if(type.getNom() == tab[1]) {
+                        efficacite.put(type,Double.parseDouble(tab[2]));break;
+                    }
+                }
+            }
+        }
+    }
 }
