6c03b2a1fe71c77c77899519f3f95c479754173f
Modjow.Jm9 <Mercandj@users.noreply.github.com>
2013-05-16 14:05:58 +0000

    Eleve grille

--- TypeTerrain.java
+++ TypeTerrain.java
@@ -5,10 +5,10 @@ import java.io.FileNotFoundException;
 import java.lang.Integer;
 
 public enum TypeTerrain {
-	FORET("foret", "foret02.png", "une foret", 2), 
-	PLAINE("plaine", "plaine03.png", "une plaine", 1), 
-	MONTAGNE("montagne", "Montagne03.png", "une montagne", 3),
-	BATIMENT("batiment", "batiment02.png", "un batiment au pif pour l'instant", 2);
+	FORET("foret", "arbres02.png", "une foret", 2), 
+	PLAINE("plaine", "plaine02.png", "une plaine", 1), 
+	MONTAGNE("montagne", "Montagne04.png", "une montagne", 3),
+	BATIMENT("batiment", "batiment01.png", "un batiment au pif pour l'instant", 2);
 	
 	private String aNom;
 	private String aImage;
