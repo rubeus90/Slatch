a50f4389b4f13cde07cab6381218f682afa84494
scalamat13@gmail.com <scalamat13@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-16 06:28:19 +0000

    TypeDeplacement

--- trunk/TypeDeplacement.java
+++ trunk/TypeDeplacement.java
@@ -3,5 +3,27 @@
  */
 public enum TypeDeplacement
 {
-	 ROUES, CHENILLES, PIED, NAVAL, AERIEN, AMPHIBIE;
+	 ROUES("roues","Les vehicules à roues ne peuvent pas aller dans les montagnes"), 
+	 CHENILLES("chenilles","Les vehicules à chenilles ne peuvent ni aller dans les montagnes ni dans les forêt"),
+	 PIED("pied","Les Unités à pied peuvent aller de partout sur terre"), 
+	 NAVAL("naval","Les Unités navales ne peuvent aller que sur l'eau"),
+	 AERIEN("aerien","Les unités aériennes peuvent aller de partout et ne sont pas influés par le terrain"), 
+	 AMPHIBIE("amphibie","Les unités amphibies peuvent aller sur terre et sur l'eau");
+	 
+	 private String aNom;
+	 private String aDescription;
+	 
+	 
+	 TypeDeplacement(final String pNom,final String pDescription){
+        aNom=pNom;
+        aDescription =pDescription;
+    }
+    
+    public String getNom(){
+        return aNom;
+    }
+    
+    public String getDescription(){
+        return aDescription;
+    }
 }
