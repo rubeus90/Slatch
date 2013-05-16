b633cbd2dec3226cd9f2354c490e863bc202c84e
RollotQuentin@gmail.com <RollotQuentin@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-16 12:49:51 +0000

    adaptation image

--- trunk/TypeTerrain.java
+++ trunk/TypeTerrain.java
@@ -8,7 +8,7 @@ public enum TypeTerrain {
 	FORET("foret", "foret02.png", "une foret", 2), 
 	PLAINE("plaine", "plaine03.png", "une plaine", 1), 
 	MONTAGNE("montagne", "Montagne03.png", "une montagne", 3),
-	BATIMENT("batiment", "batiment01.png", "un batiment au pif pour l'instant", 2);
+	BATIMENT("batiment", "batiment02.png", "un batiment au pif pour l'instant", 2);
 	
 	private String aNom;
 	private String aImage;
