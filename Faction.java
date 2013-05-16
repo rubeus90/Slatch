8849ff99eb15b1a05189385e8ac4b2b9661b9035
scalamat13 <KarlakS2@users.noreply.github.com>
2013-05-16 06:58:57 +0000

    Faction Ok

--- Faction.java
+++ Faction.java
@@ -1,15 +1,43 @@
 import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * Faction désigne un ensemble d'unités qui sont dans le même camp. Elle permet de les retrouver facilement lorsqu'on recrute.
  */
 public enum Faction
 {
-   HUMAINS, ROBOTS, ALIENS;
-   /* List<Unite> listeUnites;
+   HUMAINS("HUMAINS"), ROBOTS("ROBOTS"), ALIENS("ALIENS");
+   
+    public String aNom;
+    private List<String> aListeUnites;
     
-    Faction(List<Unite> pUnites)
+    Faction(final String pNom)
     {
-        listeUnites=pUnites;
-    }*/
+        Scanner fichier = null;
+        aNom=pNom;
+        try {
+			fichier = new Scanner(new File("Faction.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String ligne;
+		String[] tab;
+		
+		while(fichier.hasNextLine()){
+			ligne = fichier.nextLine();
+			tab = ligne.split(":");
+			if(tab[0].equals(pNom)){
+				aListeUnites.add(tab[1]);
+			}
+		}
+
+    }
+    
+    public List<String> getListe()
+    {
+        return aListeUnites;
+    }
 }
