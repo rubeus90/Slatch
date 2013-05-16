249737c7c8035b3db16e587fa4bbab939af60f80
t.patois@gmail.com <t.patois@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-16 12:27:17 +0000

    Nettoyage de Code : Partie

--- trunk/Partie.java
+++ trunk/Partie.java
@@ -31,18 +31,22 @@ public class Partie
      */
     public Partie(final int pRevenuBatiment,final int pTourMax, final String pMap)
     {
-    	try {
-			aMap = new Scanner(new File(pMap));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+        //Dans le cas ou le fichier map n'existe pas
+        try {
+            aMap = new Scanner(new File(pMap));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
         chargerMap();
-         
+        
+        
         ListeJoueur = new ArrayList<Joueur>();
         Joueur JoueurNeutre = new Joueur(0,Faction.NEUTRE,0); //Sert a occuper la place 0 dans la liste pour que le numero du joueur coresponde au numero dans la liste
         ListeJoueur.add(JoueurNeutre);
-        int i;
-        for(i=1;i<=aNbrJoueur;i++)
+        
+        //Ajout des joueur dans l'arrayList
+        for(int i=1;i<=aNbrJoueur;i++)
         {
             ListeJoueur.add(new Joueur(i,Faction.HUMAINS,batimentJoueur[i]));
         }        
@@ -53,62 +57,65 @@ public class Partie
        
     }
     
+    /**
+     * Methode qui permet le chargement d'une carte depuis un fichier texte
+     * 
+     */
     public void chargerMap(){
-    	aLargeur = Integer.parseInt(aMap.nextLine());
-		aHauteur = Integer.parseInt(aMap.nextLine());
-		aNbrJoueur = Integer.parseInt(aMap.nextLine());
-		
-		aTerrain = new Terrain[aLargeur][aHauteur];
-		
-		for(int i=0; i<aLargeur; i++){
-			for(int j=0; j<aHauteur; j++){
-				aTerrain[i][j] = new Terrain(i, j, 0, 0, TypeTerrain.PLAINE);
-			}
-		}		
-		
-		int x, y, joueur;
-		String id;
-		String ligne = "";
-		String tab[] = null;
+        aLargeur = Integer.parseInt(aMap.nextLine());
+        aHauteur = Integer.parseInt(aMap.nextLine());
+        aNbrJoueur = Integer.parseInt(aMap.nextLine());
+        
+        aTerrain = new Terrain[aLargeur][aHauteur];
+        
+        //On rempli la carte de plaine 
+        for(int i=0; i<aLargeur; i++){
+            for(int j=0; j<aHauteur; j++){
+                aTerrain[i][j] = new Terrain(i, j, 0, 0, TypeTerrain.PLAINE);
+            }
+        }       
+        
+        int x, y, joueur;
+        String id;
+        String ligne = "";
+        String tab[] = null;
 
-		batimentJoueur = new int[aNbrJoueur+1];
+        batimentJoueur = new int[aNbrJoueur+1]; //aNbrJoueur +1 pour prendre en compte le jouer Neutre
 
-		for(int i=0; i<aNbrJoueur; i++){
-			batimentJoueur[i] = 0;
-		}
-		
-		while(aMap.hasNextLine()){
-			
-			ligne = aMap.nextLine();
-			tab = ligne.split(":");
-			id = tab[0];
-			x = Integer.parseInt(tab[1]);
-			y = Integer.parseInt(tab[2]);	
-			joueur = Integer.parseInt(tab[3]);
-			
-			switch(id){
-			case "foret": aTerrain[x][y] = new Terrain(x, y, joueur, 0, TypeTerrain.FORET); break;
-			case "montagne": aTerrain[x][y] = new Terrain(x, y, joueur, 0, TypeTerrain.MONTAGNE); break;
-			case "batiment": {
-				aTerrain[x][y] = new Terrain(x, y, joueur, 10, TypeTerrain.BATIMENT); 
-				batimentJoueur[joueur]+=1;
-				break;
-			}
-			default: aTerrain[x][y] = new Terrain(x, y, joueur, 0, TypeTerrain.PLAINE);
-			}
-		}
-				
-		aTerrain[5][5].setUnite(new Unite(5,5,1,100,TypeUnite.INFANTERIE,TypeAttaque.CANON,7,1.0, "pied"));
+        //On initialise le tableau de batiment à 0 pour chaque joueur
+        for(int i=0; i<aNbrJoueur; i++){
+            batimentJoueur[i] = 0;
+        }
+        
+        //On lit le fichier et on l'analyse
+        while(aMap.hasNextLine()){
+            
+            ligne = aMap.nextLine();
+            tab = ligne.split(":");
+            id = tab[0];
+            x = Integer.parseInt(tab[1]);
+            y = Integer.parseInt(tab[2]);   
+            joueur = Integer.parseInt(tab[3]);
+            
+            switch(id){
+                case "foret": aTerrain[x][y] = new Terrain(x, y, joueur, 0, TypeTerrain.FORET); break;
+                case "montagne": aTerrain[x][y] = new Terrain(x, y, joueur, 0, TypeTerrain.MONTAGNE); break;
+                case "batiment": {
+                aTerrain[x][y] = new Terrain(x, y, joueur, 10, TypeTerrain.BATIMENT); 
+                batimentJoueur[joueur]+=1;
+                break;
+            }
+            default: aTerrain[x][y] = new Terrain(x, y, joueur, 0, TypeTerrain.PLAINE);
+            }
+        }
+        
+        //Ajout d'une unite
+        aTerrain[5][5].setUnite(new Unite(5,5,1,100,TypeUnite.INFANTERIE,TypeAttaque.CANON,7,1.0, "pied"));
     }
 
-    
-    /*************************************************************
-     * 
-     * 
-     *  ACCESSEUR
-     * 
-     * 
-     * *************************************************************/
+    /**********
+     * ACCESSEURS ET MUTATEURS
+     *********/
      
     /**
      * Accesseur qui renvoi la valeur de la hauteur du plateau
@@ -164,14 +171,14 @@ public class Partie
         return aNbrJoueur;
     }
     
-    /*
+    /**
      *Retourne un tableau comportant le nombre de batiment par joueur 
      * tab[0] correspond au joueur neutre
      * tab[1] au joueur 1
      * etc ...
      */
     public int[] getBatimentJoueur(){
-    	return batimentJoueur;
+        return batimentJoueur;
     }
     
     /**
@@ -183,16 +190,9 @@ public class Partie
         return aTerrain;
     }
     
-    /*************************************************************
-     * 
-     * 
-     *  MODIFICATEUR
-     * 
-     * 
-     * *************************************************************/
-     
+    
      /**
-     * Modificateur qui modifie la valeur du tour actuel
+     * Mutateur qui modifie la valeur du tour actuel
      * @param pTour
      */
     public void setTour(final int pTour)
@@ -201,8 +201,8 @@ public class Partie
     }
     
     /**
-     * Modificateur qui modifie la valeur du tour actuel
-     * @param pTour
+     * Mutateur qui modifie la valeur des revenues par batiment
+     * @param pRevenuBatiment
      */
     public void setRevenuBatiment(final int pRevenuBatiment)
     {
