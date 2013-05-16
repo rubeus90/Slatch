cf7ecf2664104013e11c58896c5a63afe83e0a81
martelandy764@gmail.com <martelandy764@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-16 08:09:59 +0000

    Moteur modifié

--- trunk/Moteur.java
+++ trunk/Moteur.java
@@ -8,8 +8,8 @@ class Moteur
 {  
     Unite uniteD = null; // unite en attente de déplacement
     Unite uniteA = null; // unite en attente d'attaque
-    int[][] tabDep;
-
+    int[][] tabDep; // matrice de la portée de déplacement qu'il nous reste lorsqu'on se situe sur une case, utilisée par checkPorteeDeplacement
+    HashMap<String, List<String>> chemins; // contient la liste des chemins pour arriver à la case définie par le premier String
     
     public Moteur()
     {
@@ -30,7 +30,7 @@ class Moteur
             }
         }
     }
-        public void attaque(Unite pAttaquant, Unite pVictime)
+    public void attaque(Unite pAttaquant, Unite pVictime)
     {
         double degatsAtt=0;
         degatsAtt=pAttaquant.getAttaque().degats*pAttaquant.getAttaque().efficacite.get(pVictime.getType());
@@ -57,50 +57,57 @@ class Moteur
      */
     public void caseSelectionnee(int pX, int pY)
     {
-            this.enleverSurbrillance();
-            Slatch.partie.getTerrain()[pX][pY].setSurbrillance(true);
-            Unite unite = Slatch.partie.getTerrain()[pX][pY].getUnite();
-            if(unite==null) // si une unité est présente sur la case
+        this.enleverSurbrillance();
+        Slatch.partie.getTerrain()[pX][pY].setSurbrillance(true);
+        Unite unite = Slatch.partie.getTerrain()[pX][pY].getUnite();
+        if(unite==null) // si une unité est présente sur la case
+        {
+            if(uniteD==null) // si on a sélectioné aucune unité auparavant pour le déplacement
             {
-                    if(uniteD==null) // si on a sélectioné aucune unité auparavant pour le déplacement
+                    return;
+            }
+            deplacement(uniteD, chemins.get(pX+","+pY));
+        }
+        else
+        {
+            if(uniteA==null) // si on a sélectionné aucune unité auparavant pour l'attaque
+            {
+                    if(uniteD==null)
                     {
-                            return;
+                            List<String> items= new ArrayList<String>();//on va afficher le menu en créant une liste d'items
+                            if(uniteProche(pX,pY)){items.add("Attaque");}
+                            /*if(unite.getType()==TypeUnite.INFANTERIE && Slatch.partie.getTerrain()[pX][pY].getType()==TypeTerrain.BATIMENT)
+                            {
+                                    items.add("Assaut");
+                            }*/
+                            items.add("Deplacement");
+                            Slatch.partie.getTerrain()[pX][pY].setSurbrillance(true);
+                            affichePorteeDep(unite);
+                            Slatch.ihm.getPanel().afficheMenu(items, pX, pY);
                     }
-                    //deplacement(uniteD, chemin);
             }
             else
             {
-                    if(uniteA==null) // si on a sélectionné aucune unité auparavant pour l'attaque
-                    {
-                            if(uniteD==null)
-                            {
-                                    List<String> items= new ArrayList<String>();//on va afficher le menu en créant une liste d'items
-                                    if(uniteProche(pX,pY)){items.add("Attaque");}
-                                    /*if(unite.getType()==TypeUnite.INFANTERIE && Slatch.partie.getTerrain()[pX][pY].getType()==TypeTerrain.BATIMENT)
-                                    {
-                                            items.add("Assaut");
-                                    }*/
-                                    items.add("Deplacement");
-                                    Slatch.partie.getTerrain()[pX][pY].setSurbrillance(true);
-                                    affichePorteeDep(unite);
-                                    Slatch.ihm.getPanel().afficheMenu(items, pX, pY);
-                            }
-                    }
-                    else
-                    {
-                        if(unite.getJoueur()!=uniteA.getJoueur() && uniteA.getAttaque().efficacite.containsKey(unite.getType())) // si l'unité ciblée n'appartient pas au même joueur que l'attaquant, et que l'attaquant a une attaque qui peut toucher la cible, alors on attaque
-                        {
-                                attaque(unite, uniteA);
-                        }
-                    }
+                if(unite.getJoueur()!=uniteA.getJoueur() && uniteA.getAttaque().efficacite.containsKey(unite.getType())) // si l'unité ciblée n'appartient pas au même joueur que l'attaquant, et que l'attaquant a une attaque qui peut toucher la cible, alors on attaque
+                {
+                        attaque(unite, uniteA);
+                }
             }
+        }
     }
-   
-    /*public void deplacement(Unite unite, List<"String"> chemin)
+    
+    /*
+    * permet de déplacer une unité en fonction du chemin passé en paramètre
+    */
+    public void deplacement(Unite unite, List<String> chemin)
     {
-           
-    }*/
-    public boolean uniteProche(int pX, int pY) // vérifie si une unité se trouve à côté de la case passée en paramètre
+        
+    }
+    
+    /*
+     * vérifie si une unité se trouve à côté de la case passée en paramètre
+     */
+    public boolean uniteProche(int pX, int pY)
     {
         if(pX<Slatch.partie.getLargeur())
         {
@@ -147,80 +154,115 @@ class Moteur
                 else{tabDep[i][j]=-1;} // au début, on suppose qu'on a 0 en portée de déplacement sur chacune des cases restantes
             }
         }
-       tabDep[x][y]=portee; // la position de départ de l'unité doit être initialisée avec le nombre de points de déplacement de base de l'unité
+        tabDep[x][y]=portee; // la position de départ de l'unité doit être initialisée avec le nombre de points de déplacement de base de l'unité
     }
         
     /*
      * Procedure recursive qui va déterminer les cases accessibles par une unité en focntion de sa portée de déplacement et de sa position de départ
      */
-        public void checkPorteeDeplacement(Unite unite, int porteeDep, int[] tab)
+    public void checkPorteeDeplacement(Unite unite, int porteeDep, int[] tab)
+    {
+        int k;
+        chemins = new HashMap<String, List<String>>(); // instancie la hashmap des chemins
+        for(int i=0; i<Slatch.partie.getLargeur(); i++)
         {
-            int k;
-                    if(tab[0]+1<Slatch.partie.getLargeur()) // on vérifie qu'on ne dépasse pas un bord
-                    {
-                        k= Slatch.partie.getTerrain()[tab[0]+1][tab[1]].getType().aCoutDeplacement.get(unite.getTypeDeplacement()); // k= coût de déplacement vers une case
-                        if(porteeDep-k>=0) // si on peut se déplacer sur la case, condition d'arrêt de la récursion
-                        {
-                            if(porteeDep- k >tabDep[tab[0]+1][tab[1]]) // évite des appels inutiles
-                            {
-                                int[] t = new int[2];
-                                t[0]=tab[0]+1; t[1]=tab[1]; 
-                                tabDep[tab[0]+1][tab[1]] = porteeDep -k;
-                                Slatch.partie.getTerrain()[tab[0]+1][tab[1]].setSurbrillance(true);// on met la case en question en surbrillance
-                                Slatch.ihm.getPanel().dessineTerrain(tab[0]+1,tab[1]);
-                                checkPorteeDeplacement(unite, porteeDep-k, t); // et on appelle à nouveau la procédure en changeant de place et en faisant décroître la portée de déplacement
-                            }
-                        }
-                    }
-                    if(tab[1]+1<Slatch.partie.getHauteur())// on vérifie qu'on ne dépasse pas un bord
-                    {
-                        k= Slatch.partie.getTerrain()[tab[0]][tab[1]+1].getType().aCoutDeplacement.get(unite.getTypeDeplacement());
-                        if(porteeDep-k>=0) // si on peut se déplacer sur la case
-                        {
-                            if(porteeDep- k >tabDep[tab[0]][tab[1]+1])
-                            {
-                                int[] t = new int[2];
-                                t[0]=tab[0]; t[1]=tab[1]+1;
-                                tabDep[tab[0]][tab[1]+1] = porteeDep -k;
-                                Slatch.partie.getTerrain()[tab[0]][tab[1]+1].setSurbrillance(true);
-                                Slatch.ihm.getPanel().dessineTerrain(tab[0],tab[1]+1);
-                                checkPorteeDeplacement(unite, porteeDep-k, t);
-                            }
-                        }
-                    }
-                    if(tab[0]>0)// on vérifie qu'on ne dépasse pas un bord
-                    {
-                        k= Slatch.partie.getTerrain()[tab[0]-1][tab[1]].getType().aCoutDeplacement.get(unite.getTypeDeplacement());
-                        if(porteeDep-k>=0) // si on peut se déplacer sur la case
-                        {
-                            if(porteeDep- k >tabDep[tab[0]-1][tab[1]])
-                            {
-                                int[] t = new int[2];
-                                t[0]=tab[0]-1; t[1]=tab[1];
-                                tabDep[tab[0]-1][tab[1]] = porteeDep -k;
-                                Slatch.partie.getTerrain()[tab[0]-1][tab[1]].setSurbrillance(true);
-                                Slatch.ihm.getPanel().dessineTerrain(tab[0]-1,tab[1]);
-                                checkPorteeDeplacement(unite, porteeDep-k, t);
-                            }
-                        }
-                    }
-                    if(tab[1]>0)// on vérifie qu'on ne dépasse pas un bord
-                    {
-                        k= Slatch.partie.getTerrain()[tab[0]][tab[1]-1].getType().aCoutDeplacement.get(unite.getTypeDeplacement());
-                        if(porteeDep-k>=0) // si on peut se déplacer sur la case
-                        {
-                            if(porteeDep- k >tabDep[tab[0]][tab[1]-1])
-                            {
-                                int[] t = new int[2];
-                                t[0]=tab[0]; t[1]=tab[1]-1;
-                                tabDep[tab[0]][tab[1]-1] = porteeDep -k;
-                                Slatch.partie.getTerrain()[tab[0]][tab[1]-1].setSurbrillance(true);
-                                Slatch.ihm.getPanel().dessineTerrain(tab[0],tab[1]-1);
-                                checkPorteeDeplacement(unite, porteeDep-k, t);
-                            }
-                        }
-                    }               
-                    
-                
+            for(int j=0; j<Slatch.partie.getHauteur(); j++)
+            {
+                chemins.put(i+","+j, new ArrayList<String>()); // instancie chaque chemin
+            }
         }
+        
+        /*
+         * Vers la droite
+         */
+        if(tab[0]+1<Slatch.partie.getLargeur()) // on vérifie qu'on ne dépasse pas un bord
+        {
+            k= Slatch.partie.getTerrain()[tab[0]+1][tab[1]].getType().aCoutDeplacement.get(unite.getTypeDeplacement()); // k= coût de déplacement vers une case
+            if(porteeDep-k>=0) // si on peut se déplacer sur la case, condition d'arrêt de la récursion
+            {
+                if(porteeDep- k >tabDep[tab[0]+1][tab[1]]) // évite des appels inutiles
+                {
+                    int[] t = new int[2];
+                    t[0]=tab[0]+1; t[1]=tab[1]; 
+                    tabDep[tab[0]+1][tab[1]] = porteeDep -k;// actualise la portée de deplacement restante sur la case correspondante de la matrice tabDep
+                    chemins.get((tab[0]+1)+","+tab[1]).add(tab[0]+","+tab[1]);
+                    if(!Slatch.partie.getTerrain()[tab[0]+1][tab[1]].getSurbrillance()) // si la case n'a pas déjà été mise en valeur
+                    {
+                        Slatch.partie.getTerrain()[tab[0]+1][tab[1]].setSurbrillance(true);// on met la case en question en surbrillance
+                        Slatch.ihm.getPanel().dessineTerrain(tab[0]+1,tab[1]);
+                    }
+                    checkPorteeDeplacement(unite, porteeDep-k, t); // et on appelle à nouveau la procédure en changeant de place et en faisant décroître la portée de déplacement
+                }
+            }
+        }
+        /*
+         * Vers le bas
+         */
+        if(tab[1]+1<Slatch.partie.getHauteur())// on vérifie qu'on ne dépasse pas un bord
+        {
+            k= Slatch.partie.getTerrain()[tab[0]][tab[1]+1].getType().aCoutDeplacement.get(unite.getTypeDeplacement());
+            if(porteeDep-k>=0) // si on peut se déplacer sur la case
+            {
+                if(porteeDep- k >tabDep[tab[0]][tab[1]+1])
+                {
+                    int[] t = new int[2];
+                    t[0]=tab[0]; t[1]=tab[1]+1;
+                    tabDep[tab[0]][tab[1]+1] = porteeDep -k;
+                    chemins.get(tab[0]+","+(tab[1]+1)).add(tab[0]+","+tab[1]);
+                    if(!Slatch.partie.getTerrain()[tab[0]][tab[1]+1].getSurbrillance())
+                    {
+                        Slatch.partie.getTerrain()[tab[0]][tab[1]+1].setSurbrillance(true);
+                        Slatch.ihm.getPanel().dessineTerrain(tab[0],tab[1]+1);
+                    }
+                    checkPorteeDeplacement(unite, porteeDep-k, t);
+                }
+            }
+        }
+        /*
+         * Vers la gauche
+         */
+        if(tab[0]>0)// on vérifie qu'on ne dépasse pas un bord
+        {
+            k= Slatch.partie.getTerrain()[tab[0]-1][tab[1]].getType().aCoutDeplacement.get(unite.getTypeDeplacement());
+            if(porteeDep-k>=0) // si on peut se déplacer sur la case
+            {
+                if(porteeDep- k >tabDep[tab[0]-1][tab[1]])
+                {
+                    int[] t = new int[2];
+                    t[0]=tab[0]-1; t[1]=tab[1];
+                    tabDep[tab[0]-1][tab[1]] = porteeDep -k;
+                    chemins.get((tab[0]-1)+","+tab[1]).add(tab[0]+","+tab[1]);
+                    if(!Slatch.partie.getTerrain()[tab[0]-1][tab[1]].getSurbrillance())
+                    {
+                        Slatch.partie.getTerrain()[tab[0]-1][tab[1]].setSurbrillance(true);
+                        Slatch.ihm.getPanel().dessineTerrain(tab[0]-1,tab[1]);
+                    }
+                    checkPorteeDeplacement(unite, porteeDep-k, t);
+                }
+            }
+        }
+        /*
+         * Vers le haut
+         */
+        if(tab[1]>0)// on vérifie qu'on ne dépasse pas un bord
+        {
+            k= Slatch.partie.getTerrain()[tab[0]][tab[1]-1].getType().aCoutDeplacement.get(unite.getTypeDeplacement());
+            if(porteeDep-k>=0) // si on peut se déplacer sur la case
+            {
+                if(porteeDep- k >tabDep[tab[0]][tab[1]-1])
+                {
+                    int[] t = new int[2];
+                    t[0]=tab[0]; t[1]=tab[1]-1;
+                    tabDep[tab[0]][tab[1]-1] = porteeDep -k;
+                    chemins.get(tab[0]+","+(tab[1]-1)).add(tab[0]+","+tab[1]);
+                    if(!Slatch.partie.getTerrain()[tab[0]][tab[1]-1].getSurbrillance())
+                    {
+                        Slatch.partie.getTerrain()[tab[0]][tab[1]-1].setSurbrillance(true);
+                        Slatch.ihm.getPanel().dessineTerrain(tab[0],tab[1]-1);
+                    }
+                    checkPorteeDeplacement(unite, porteeDep-k, t);
+                }
+            }
+        }                
+    }
 }
