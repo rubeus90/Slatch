95beb02ed6ce9dca262e3386c746a27722d06ece
t.patois <7h1b0@users.noreply.github.com>
2013-05-22 16:03:01 +0000

    Ajout Prix TypeUnite

--- TypeUnite.java
+++ TypeUnite.java
@@ -3,22 +3,24 @@
  */
 public enum TypeUnite
 {
-    COMMANDO("commando", "une infanterie","commando"), 
+    COMMANDO("commando", "une infanterie","commando",100), 
     //DEMOLISSEUR("demolisseur", "une infanterie","demolisseur"), 
     //CHAR("char","un char","char"),
-    TANK("tank","un tank","tank"),
+    TANK("tank","un tank","tank",500),
     //INGENIEUR("ingenieur","un ingenieur formidable","ingenieur"),
     //DISTANCE("distance","un vehicule a moyenne distance","distance"),
-    UML("uml","Ultmiate missile launcher","uml");
+    UML("uml","Ultmiate missile launcher","uml",300);
 
     private String aNom;
     private String aDescription;
     private String aImage;
+    private int aPrix
     
-    TypeUnite(final String pNom,final String pDescription,final String pImage){
+    TypeUnite(final String pNom,final String pDescription,final String pImage,final int pPrix){
         aNom=pNom;
         aDescription =pDescription;
         aImage = pImage;
+        aPrix = pPrix;
     }
     
     /**
@@ -43,6 +45,14 @@ public enum TypeUnite
      */
     public String getImage(){
         return aImage;
+    }
+    
+    /**
+     * Accesseur pour aPrix
+     * @return aPrix
+     */
+    public int getPrix(){
+        return aPrix;
     }
 
 }
