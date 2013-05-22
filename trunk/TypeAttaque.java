a33a49b7c448e5d2002bd9a3e2d11fe000245630
t.patois@gmail.com <t.patois@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-22 12:17:32 +0000

    AJout TypeAttaque Roquette

--- trunk/TypeAttaque.java
+++ trunk/TypeAttaque.java
@@ -8,6 +8,7 @@ public enum TypeAttaque
     OMEGA_SLASH_DE_L_ULTIME_APOLLON(250, "Omega Slash de l'ultime Apollon", "Une série de coups frénétiques qui laisseront tous les ennemis sur le tapis",PorteeAttaque.COURTE), // en attente de créer les vraies attaques
     CANON(15, "Tir de canon", "Envoie un obus sur votre ennemi. Efficace contre l'infanterie.",PorteeAttaque.COURTE),
     FUSIL(10, "Tir de fusil", "Envoie une salve de balles sur votre ennemi.",PorteeAttaque.COURTE),
+    ROQUETTE(15,"Tir de roquette","Envoie des roquettes sur votre ennemi",PorteeAttaque.MOYENNE),
     MISSILE(15,"Tir de missile","Envoie des missiles sur votre ennemi.", PorteeAttaque.LONGUE);
    
     private int aDegats; // dégâts de base de l'attaque
