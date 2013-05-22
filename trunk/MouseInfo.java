a3baf42c830b00f8e64e2189a0d988ac99bf47c7
Modjow.Jm9@gmail.com <Modjow.Jm9@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-22 13:01:55 +0000

    ATTENTION CHANGEMENT DANGEREUX svn add IHM_NEW.java PanelMatrice.java MouseInfo.java MouseMatrice.javasvn add IHM_NEW.java PanelMatrice.java MouseInfo.java MouseMatrice.java(nouvelle IHM)

---
+++ trunk/MouseInfo.java
@@ -0,0 +1,32 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+/**
+ * Write a description of class Mouse_Info here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MouseInfo implements MouseListener
+{
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        Slatch.ihm.getpanelinfo().coordclickUnite(event.getX(),event.getY());
+    }
+  
+    @Override
+    public void mouseEntered(MouseEvent event) {
+    }
+       
+    @Override
+    public void mouseExited(MouseEvent event) {
+    }
+  
+    @Override
+    public void mousePressed(MouseEvent event) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+        
+    }  
+}
