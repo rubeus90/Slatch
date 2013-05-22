74be467123f0ebd9c4b131f3178710e59b63f9ef
Modjow.Jm9@gmail.com <Modjow.Jm9@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-22 15:56:21 +0000

    Affiche le menu description

--- trunk/PanelInfo.java
+++ trunk/PanelInfo.java
@@ -54,74 +54,68 @@ public class PanelInfo extends JPanel
         // Bouton SUIVANT fonctionnement
         if(0<pY && pY<this.getHeight() && this.getWidth()-6*this.getWidth()/32<pX && pX<this.getWidth()) 
         {
-           // System.out.println("coucou");
+            // Efface le petit menu 
+            Slatch.ihm.getPanel().setMenuUniteAction(false);
+            // Efface le menu
+            Slatch.ihm.getPanel().setMenuUniteDescription(false);
+            // Avertir Moteur
+            Slatch.moteur.passeTour();
+            this.repaint();
+            Slatch.ihm.getPanel().repaint();
             
-//             
-//             // Efface le petit menu
-               Slatch.ihm.getPanel().setMenuUnite(false);
-//             effaceMenuUnite(g);
-//             // Efface le menu
-               Slatch.ihm.getPanel().setMenu(false);
-//             // Avertir Moteur
-              Slatch.moteur.passeTour();
-              this.repaint();
-              Slatch.ihm.getPanel().repaint();
-              
-//             
-//             // Joueur Actuel affichage
-//             afficheImageRedim ("joueur.png", 18*aLargeurCarreau, 0,19*aLargeurCarreau, DECALAGE_PX_EN_Y, g);
-//             for(int i=0; i<10;i++) {
-//                 if(Slatch.partie.getJoueurActuel()%10==i) {
-//                     afficheImageRedim ("numero"+i+".png", 19*aLargeurCarreau, DECALAGE_PX_EN_Y/4,20*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
-//                 }
-//             }
-//             
-//             // Jour Actuel affichage
-//             afficheImageRedim ("jour.png", 3*aLargeurCarreau, 0,7*aLargeurCarreau, DECALAGE_PX_EN_Y, g);
-//             afficheImageRedim ("menu.png", 0, 0,4*aLargeurCarreau, DECALAGE_PX_EN_Y, g);
-//             // Affiche les dizaines
-//             for(int i=0; i<10;i++) {
-//                 if(Slatch.partie.getTour()%10==i) {
-//                     afficheImageRedim ("numero"+i+".png", 8*aLargeurCarreau, DECALAGE_PX_EN_Y/4,9*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
-//                 }
-//             }
-//             // Affiche les unites
-//             for(int i=0; i<10;i++) {
-//                 if(Slatch.partie.getTour()/10%10==i) {
-//                     afficheImageRedim ("numero"+i+".png", 7*aLargeurCarreau,DECALAGE_PX_EN_Y/4,8*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
-//                 }
-//             }
-//             
-//             
-//             // Argent du joueur actuel affichage
-//             afficheImageRedim ("argent.png", 9*aLargeurCarreau, 0,13*aLargeurCarreau, DECALAGE_PX_EN_Y, g);
-//             // Affiche les unites
-//             for(int u=0; u<10;u++) {
-//                 if(Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getArgent()%10==u) {
-//                     afficheImageRedim ("numero"+u+".png", 16*aLargeurCarreau, DECALAGE_PX_EN_Y/4,17*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
-//                 }
-//             }
-//             // Affiche les dizaines
-//             for(int v=0; v<10;v++) {
-//                 if((Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getArgent()/10)%10==v) {
-//                     afficheImageRedim ("numero"+v+".png", 15*aLargeurCarreau,DECALAGE_PX_EN_Y/4,16*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
-//                 }
-//             }
-//             // Affiche les centaines
-//             for(int v=0; v<10;v++) {
-//                 if((Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getArgent()/100)%10==v) {
-//                     afficheImageRedim ("numero"+v+".png", 14*aLargeurCarreau,DECALAGE_PX_EN_Y/4,15*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
-//                 }
-//             }
-//             // Affiche les milliemes
-//             for(int v=0; v<10;v++) {
-//                 if((Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getArgent()/1000)%10==v) {
-//                     afficheImageRedim ("numero"+v+".png", 13*aLargeurCarreau,DECALAGE_PX_EN_Y/4,14*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
-//                 }
-//             }
+            /*
+            // Joueur Actuel affichage
+            afficheImageRedim ("joueur.png", 18*aLargeurCarreau, 0,19*aLargeurCarreau, DECALAGE_PX_EN_Y, g);
+            for(int i=0; i<10;i++) {
+                if(Slatch.partie.getJoueurActuel()%10==i) {
+                    afficheImageRedim ("numero"+i+".png", 19*aLargeurCarreau, DECALAGE_PX_EN_Y/4,20*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
+                }
+            }
+            
+            // Jour Actuel affichage
+            afficheImageRedim ("jour.png", 3*aLargeurCarreau, 0,7*aLargeurCarreau, DECALAGE_PX_EN_Y, g);
+            afficheImageRedim ("menu.png", 0, 0,4*aLargeurCarreau, DECALAGE_PX_EN_Y, g);
+            // Affiche les dizaines
+            for(int i=0; i<10;i++) {
+                if(Slatch.partie.getTour()%10==i) {
+                    afficheImageRedim ("numero"+i+".png", 8*aLargeurCarreau, DECALAGE_PX_EN_Y/4,9*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
+                }
+            }
+            // Affiche les unites
+            for(int i=0; i<10;i++) {
+                if(Slatch.partie.getTour()/10%10==i) {
+                    afficheImageRedim ("numero"+i+".png", 7*aLargeurCarreau,DECALAGE_PX_EN_Y/4,8*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
+                }
+            }
+            
+            
+            // Argent du joueur actuel affichage
+            afficheImageRedim ("argent.png", 9*aLargeurCarreau, 0,13*aLargeurCarreau, DECALAGE_PX_EN_Y, g);
+            // Affiche les unites
+            for(int u=0; u<10;u++) {
+                if(Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getArgent()%10==u) {
+                    afficheImageRedim ("numero"+u+".png", 16*aLargeurCarreau, DECALAGE_PX_EN_Y/4,17*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
+                }
+            }
+            // Affiche les dizaines
+            for(int v=0; v<10;v++) {
+                if((Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getArgent()/10)%10==v) {
+                    afficheImageRedim ("numero"+v+".png", 15*aLargeurCarreau,DECALAGE_PX_EN_Y/4,16*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
+                }
+            }
+            // Affiche les centaines
+            for(int v=0; v<10;v++) {
+                if((Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getArgent()/100)%10==v) {
+                    afficheImageRedim ("numero"+v+".png", 14*aLargeurCarreau,DECALAGE_PX_EN_Y/4,15*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
+                }
+            }
+            // Affiche les milliemes
+            for(int v=0; v<10;v++) {
+                if((Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getArgent()/1000)%10==v) {
+                    afficheImageRedim ("numero"+v+".png", 13*aLargeurCarreau,DECALAGE_PX_EN_Y/4,14*aLargeurCarreau, 3*DECALAGE_PX_EN_Y/4, g);
+                }
+            }*/
         }
-        
-        
     }
     
     /**
