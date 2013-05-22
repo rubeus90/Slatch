be7a9357e8147cddbb0420e94a92dc828bfc0117
martelandy764@gmail.com <martelandy764@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-22 15:02:09 +0000

    Ajout d'une petite méthode dans la classe UniteIA qui fera se diriger une unité vers un point de la carte

--- trunk/UniteIA.java
+++ trunk/UniteIA.java
@@ -2,46 +2,9 @@ import java.awt.Point;
 import java.util.Stack;
 public class UniteIA
 {
-    public void seDirigerVers(Unite unite, Point pP)
+    public void seDirigerVers(Unite unite, int pX, int pY)
     {
-        Slatch.moteur.remplitPorteeDep(unite);
-        
-        boolean fini = false;
-        int x = (int)pP.getX(), y =(int)pP.getY();
-        Stack<Point> chemin = new Stack<Point>();
-        chemin.push(pP);
-        while(!fini)
-        {
-            Point p = Slatch.moteur.pred[x][y];
-            x=(int)p.getX();
-            y=(int)p.getY();
-            if(unite.getCoordonneeX()==x && unite.getCoordonneeY()==y)
-            {
-                fini = true;
-            }
-            else
-            {
-                chemin.push(p);
-            }
-        }
-        int k = chemin.size();
-        int l = unite.getPorteeDeplacement();
-        while(!chemin.isEmpty())
-        {
-            Point p = chemin.pop();
-            if((l-=Slatch.partie.getTerrain()[(int)p.getX()][(int)p.getY()].getCout(unite))<0)
-            {
-                break;
-            }
-            Slatch.moteur.changerCase(unite, (int)p.getX(), (int)p.getY());
-            try{
-                Thread.sleep(250/k+50);
-            }
-            catch(InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
-        unite.deplacee(true);
+        Slatch.moteur.remplitPorteeDep(unite, false);
+        Slatch.moteur.deplacement(unite, pX, pY);
     }
 }
