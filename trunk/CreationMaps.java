26ef9bc695c3dee308636ccbda2c658106c29201
nghongngoc90@gmail.com <nghongngoc90@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-24 09:21:32 +0000

    Debut Map Creator (pas fini)

--- trunk/CreationMaps.java
+++ trunk/CreationMaps.java
@@ -2,28 +2,50 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class CreationMaps extends JPanel{
+public class CreationMaps extends JPanel implements ActionListener {
 	public JFrame frame;
 	public JPanel panel;
-	public PanelMatrice panelMap;
+	public PanelMap panelMap;
 	public JPanel panelTerrain;
 	private int NbrCaseX;
     private int NbrCaseY;
     private Terrain[][] MATRICE_TEST;
-    private Partie partie;
+    private Partie partieNew;
+    private JButton buttonMontagne;
+    private JButton buttonBatiment;
+    private JButton buttonForet;
+    private JButton buttonRouteVertical;
+    private JButton buttonRouteHorizontal;
+    private JButton buttonRouteCarrefour;
+    private JButton buttonRouteDroiteBas;
+    private JButton buttonRouteDroiteHaut;
+    private JButton buttonRouteGaucheBas;
+    private JButton buttonRouteGaucheHaut;
+    private JButton buttonRouteTHaut;
+    private JButton buttonRouteTBas;
+    private JButton buttonRouteTGauche;
+    private JButton buttonRouteTDroite;
+    private JButton buttonJoueur1;
+    private JButton buttonJoueur2;
+    
+    private String aID;
+    private String aX;
+    private String aY;
+    private String aJoueur;
 	
 	public CreationMaps(){
 		
-		partie = new Partie(20, 30, "Maps/mapGenere.txt");
-		
-		NbrCaseX = partie.getLargeur();
-        NbrCaseY = partie.getHauteur();
-        MATRICE_TEST=partie.getTerrain();
+		partieNew = new Partie(25,35,"Maps/mapGenere.txt");
+		NbrCaseX = partieNew.getLargeur();
+        NbrCaseY = partieNew.getHauteur();
+        MATRICE_TEST=partieNew.getTerrain();
 		
 		frame = new JFrame("Creation maps");
 		
@@ -31,7 +53,7 @@ public class CreationMaps extends JPanel{
 				
 		frame.setContentPane(panel);
 				
-		panelMap = new PanelMatrice();
+		panelMap = new PanelMap(partieNew);
 		panelMap.setPreferredSize(new Dimension(802,524));
 		
 		panelTerrain = new JPanel();
@@ -55,6 +77,8 @@ public class CreationMaps extends JPanel{
 		JButton buttonRouteTBas= new JButton("Route en T bas");
 		JButton buttonRouteTGauche= new JButton("Route en T gauche");
 		JButton buttonRouteTDroite= new JButton("Route en T droite");
+		JButton buttonJoueur1 = new JButton("Joueur 1");
+		JButton buttonJoueur2 = new JButton("Joueur 2");
 				
 		panelTerrain.setLayout(new GridLayout(2,7));
 		panelTerrain.add(buttonForet);
@@ -71,22 +95,46 @@ public class CreationMaps extends JPanel{
 		panelTerrain.add(buttonRouteTGauche);		
 		panelTerrain.add(buttonRouteTHaut);
 		panelTerrain.add(buttonRouteVertical);
+		panelTerrain.add(buttonJoueur1);
+		panelTerrain.add(buttonJoueur2);
+		
+		buttonMontagne.addActionListener(this);
+		buttonBatiment.addActionListener(this);
+		buttonForet.addActionListener(this);
+		buttonRouteVertical.addActionListener(this);
+		buttonRouteHorizontal.addActionListener(this);
+		buttonRouteCarrefour.addActionListener(this);
+		buttonRouteDroiteBas.addActionListener(this);
+		buttonRouteDroiteHaut.addActionListener(this);
+		buttonRouteGaucheBas.addActionListener(this);
+		buttonRouteGaucheHaut.addActionListener(this);
+		buttonRouteTHaut.addActionListener(this);
+		buttonRouteTBas.addActionListener(this);
+		buttonRouteTGauche.addActionListener(this);
+		buttonRouteTDroite.addActionListener(this);
+		buttonJoueur1.addActionListener(this);
+		buttonJoueur2.addActionListener(this);
 		
 		frame.pack();
 		frame.setVisible(true);
 	}
 	
-	private void dessineMatrice(final Terrain[][] pMatrice, final Graphics g, PanelMatrice pPanel) {
-        for(int i=0; i<NbrCaseX; i++) {
-            for(int j=0; j<NbrCaseY; j++) {
-                partie.getTerrain()[i][j].dessine(g, pPanel);
-            }
-        }
-    }
 	
 	@Override
     public void paintComponent (final Graphics g) 
     {
-        dessineMatrice(MATRICE_TEST, g, panelMap);
-    }    
+        panelMap.dessineMatrice(g);
+    }
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand() == "Joueur 1")
+			aJoueur = "1";
+			switch(e.getActionCommand()){
+			case "montagne": {
+				aID = "montagne";
+			}
+		}
+	}    
 }
