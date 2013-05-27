ceb4859cd53388edf3c82a021b31d7db38944a89
rubeus <rubeus90@gmail.com>
2013-05-27 15:28:18 +0000

    Retirer sauvegardePartie() du constructeur de Partie

--- Partie.java
+++ Partie.java
@@ -49,9 +49,6 @@ public class Partie
         aTour = 1;
         aRevenuBatiment = pRevenuBatiment;
         ListeJoueur.get(1).benefTour(aRevenuBatiment);
-        
-        
-        sauvegardePartie();
     }
     
     /**
