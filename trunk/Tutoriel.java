aca6cd8ab5343f2ce739ed8ff3b321115a2f067b
scalamat13@gmail.com <scalamat13@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-27 06:48:09 +0000

    debut de la classe tuto ( y'a rien dedans )

---
+++ trunk/Tutoriel.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class Tutoriel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tutoriel extends Partie
+{
+    /* ATTRIBUT VENANT DE PARTIE
+    private int aNbrJoueur;
+    private List<Joueur> ListeJoueur;
+    private int aRevenuBatiment;
+    private int aLargeur;
+    private int aHauteur;
+    private int aTourMax;
+    private Scanner aMap;
+    private Terrain[][] aTerrain;
+    private int aJoueurActuel;
+    private int aTour;
+    */
+    
+    private int aEtape;
+    
+    
+    /**
+     * Constructor for objects of class Tutoriel
+     */
+    public Tutoriel(final int pRevenuBatiment,final int pTourMax, final String pMap)
+    {
+       super(pRevenuBatiment,pTourMax,pMap);
+       aEtape = 1;
+    }
+
+    public void deroulement()
+    {
+        switch(aEtape)
+        {
+            case 1 :
+        }
+        
+    }
+}
