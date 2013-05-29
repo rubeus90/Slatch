da5805aea46085e47193190fadecb9914e5ebcbd
rubeus <rubeus90@gmail.com>
2013-05-29 12:42:58 +0000

    Cleanup code

--- CreationMaps.java
+++ CreationMaps.java
@@ -1,16 +1,10 @@
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.Point;
-import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.BufferedWriter;
@@ -20,7 +14,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -32,8 +25,6 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.JToggleButton;
-import javax.swing.border.Border;
 
 public class CreationMaps extends JPanel implements ActionListener,
 		MouseListener {
@@ -203,8 +194,6 @@ public class CreationMaps extends JPanel implements ActionListener,
 			Point point = panelMap.coordclickUnite(e.getX(), e.getY());
 			Integer pX = (int) point.getX();
 			Integer pY = (int) point.getY();
-			String X = pX.toString();
-			String Y = pY.toString();
 
 			partieNew.setCarreauTerrain(pX,pY,new Terrain(pX, pY,
 					Integer.parseInt(aJoueur), TypeTerrain.valueOf(ID)));
