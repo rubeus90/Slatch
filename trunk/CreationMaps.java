91976afceac7b3333323c9d87e562e9094854d9f
nghongngoc90@gmail.com <nghongngoc90@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-29 15:28:26 +0000

    Revert a une version precedente de Map creator

--- trunk/CreationMaps.java
+++ trunk/CreationMaps.java
@@ -1,6 +1,5 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
@@ -18,7 +17,6 @@ import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -28,9 +26,9 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.ListCellRenderer;
 
-public class CreationMaps extends JPanel implements ActionListener,	MouseListener {
+public class CreationMaps extends JPanel implements ActionListener,
+		MouseListener {
 	private JFrame frame;
 	private JPanel panel;
 	private PanelMap panelMap;
@@ -58,13 +56,14 @@ public class CreationMaps extends JPanel implements ActionListener,	MouseListene
 	private JTextField largeur;
 	private JTextField hauteur;
 	private JTextField nbrJoueur;
-	private JTextField nomMap;
 
 	private String aLargeur;
 	private String aHauteur;
 	private String aNbrJoueur;
+//	private String ID;
+//	private String aX;
+//	private String aY;
 	private String aJoueur;
-	private String aNomMap;
 
 	public CreationMaps() {		
 		createDialog();
@@ -229,28 +228,13 @@ public class CreationMaps extends JPanel implements ActionListener,	MouseListene
 	}
 
 	public void genererMap() {
-		partieNew.sauvegardePartie("Maps/mapGenere2.txt");
+		partieNew.sauvegardePartie("Maps/mapGenere.txt");
 	}
 
-	public void initialiseMap() {	
-//		File fichier = new File("Maps/mapGenere2.txt");
-//		try {
-//			fichier.createNewFile();
-//		} catch (IOException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		
-//		fichier.setReadable(true);
-//		fichier.setWritable(true);
-//		fichier.setExecutable(true);
-		
-		
+	public void initialiseMap() {		
 		try {
-			File file = new File(getClass().getClassLoader().getResource("Maps/mapGenere2.txt").toURI());
-			if(!file.exists())
-				file.createNewFile();
-			
+			File file = new File(getClass().getClassLoader()
+					.getResource("Maps/mapGenere.txt").toURI());
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			fw.write("");
 			BufferedWriter bw = new BufferedWriter(fw);
@@ -281,7 +265,6 @@ public class CreationMaps extends JPanel implements ActionListener,	MouseListene
 	}
 
 	public void createDialog() {
-		
 		largeur = new JTextField(3);
 		hauteur = new JTextField(3);
 		nbrJoueur = new JTextField(2);
@@ -323,5 +306,5 @@ public class CreationMaps extends JPanel implements ActionListener,	MouseListene
 		else {
 			System.exit(0);
 		}
-	}
+	}		
 }
