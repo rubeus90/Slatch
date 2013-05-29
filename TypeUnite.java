69bf5e60df9a6b256508737480f7b005e2be6f27
t.patois <7h1b0@users.noreply.github.com>
2013-05-29 12:10:26 +0000

    Suppression des images

--- TypeUnite.java
+++ TypeUnite.java
@@ -5,8 +5,8 @@ public enum TypeUnite
 {
     COMMANDO("Commando", "Capable de capturer un batiment","commando",100,30,40,"fusil",TypeDeplacement.PIED,1.0, "Terrestre"),
     DEMOLISSEUR("Demolisseur", "Capable capturer un batiment de faire des degats aux tanks","demolisseur",200,20,30,"mortier",TypeDeplacement.PIED,1.0,"Terrestre"), 
-    CHAR("Char","un char","tank",300,40,60,"canon",TypeDeplacement.CHENILLES,1.0,"Terrestre"),
-    TANK("Tank","Unite tres puissante","while",700,65,40,"groscanon",TypeDeplacement.CHENILLES,1.0, "Terrestre"),
+    CHAR("Char","un char","char",300,40,60,"canon",TypeDeplacement.CHENILLES,1.0,"Terrestre"),
+    TANK("Tank","Unite tres puissante","tank",700,65,40,"groscanon",TypeDeplacement.CHENILLES,1.0, "Terrestre"),
     INGENIEUR("Ingenieur","Capable de soigner des unites et de les faire evoluer","ingenieur",100,15,40,"soin",TypeDeplacement.PIED,1.0, "Terrestre"),
     DISTANCE("Distance","un vehicule a moyenne distance","distance",350,35,50,"roquette",TypeDeplacement.ROUES,1.0, "Terrestre"),
     UML("Uml","Capable de tirer a distance","uml",450,30,30,"missile",TypeDeplacement.CHENILLES,1.0, "Terrestre");
