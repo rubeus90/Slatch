4d5dad69c628b2a56e86a759d0d75822c2ed734d
t.patois@gmail.com <t.patois@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-05-30 12:19:42 +0000

    Test

--- trunk/Slatch.java
+++ trunk/Slatch.java
@@ -30,7 +30,7 @@ public class Slatch {
         aImages=new HashMap<String,Image>();
         loadImage();
 
-        Partie partieRapide = new Partie(20,30,"Maps/mapTest.txt");
+        Partie partieRapide = new Partie(20,30,"Maps/mapTest4.txt");
         //partie = new Partie("Maps/mapGenere.txt");
         //partie =new Partie("Maps/sauvegarde.txt");
   //      Tutoriel tuto = new Tutoriel("Maps/tutoriel.txt");
