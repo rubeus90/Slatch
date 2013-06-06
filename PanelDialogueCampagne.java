0d9d581e8d47cdcfdfe5d22994443119e12969e1
rubeus <rubeus90@gmail.com>
2013-06-06 16:41:04 +0000

    Changer l'apparence du dialogue

--- PanelDialogueCampagne.java
+++ PanelDialogueCampagne.java
@@ -1,46 +1,63 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import javax.swing.JPanel;
 
 /**
  * @author Ngoc
- *
+ * 
  */
-public class PanelDialogueCampagne extends JPanel{
-	
+public class PanelDialogueCampagne extends JPanel {
+
 	private String dialogue;
 	private int etape;
 	private int niveau;
-	
-	public PanelDialogueCampagne(final int pNiveau){
+	private Thread t;
+
+	public PanelDialogueCampagne(final int pNiveau) {
 		super();
 		etape = 1;
 		niveau = pNiveau;
-		dialogue = "Coucoucoucou on va commencer le niveau " + (pNiveau+1);
+		dialogue = "Coucoucoucou on va commencer le niveau " + (pNiveau + 1);
 	}
-	
+
 	@Override
-	public void paintComponent(final Graphics g){
-		afficheImageRedim ("noir", 0, 0, this.getWidth(), this.getHeight(), g);
-		
-		Font font = new Font("Serif", Font.BOLD, this.getWidth()/75);
-        g.setFont(font);
-        FontMetrics fm = getFontMetrics(font); 
-        g.setColor(Color.white);
-        
-        int hauteurSize = fm.getHeight();
-        int joueurSize = fm.stringWidth(dialogue);
-        g.drawString(dialogue, this.getWidth()/2 - joueurSize/2, 2*this.getHeight()/(2*hauteurSize));
+	public void paintComponent(final Graphics g) {
+		afficheImageRedim("noir", 0, 0, this.getWidth(), this.getHeight(), g);
+
+		Font font;
+		try {
+			font = Font.createFont(Font.TRUETYPE_FONT, new File(getClass()
+					.getClassLoader().getResource("Config/BlackOps.ttf")
+					.toURI())).deriveFont(Font.PLAIN, 20f);
+			g.setFont(font);
+			g.setColor(Color.white);
+//			FontMetrics fm = getFontMetrics(font);
+//			int hauteurSize = fm.getHeight();
+//			int longueur = fm.stringWidth(dialogue);
+			g.drawString(dialogue, this.getWidth() / 12, this.getHeight() / 2);
+		} catch (FontFormatException | IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+		// Font font = new Font("Serif", Font.BOLD, this.getWidth()/75);
+
 	}
-	
-	private void afficheImageRedim (final String pURL, final int pPosHautGaucheX, final int pPosHautGaucheY,final int pPosBasDroiteX, final int pPosBasDroiteY, final Graphics g) {  
+
+	private void afficheImageRedim(final String pURL,
+			final int pPosHautGaucheX, final int pPosHautGaucheY,
+			final int pPosBasDroiteX, final int pPosBasDroiteY, final Graphics g) {
 		Image img = Slatch.aImages.get(pURL);
-		g.drawImage(img, pPosHautGaucheX, pPosHautGaucheY, pPosBasDroiteX-pPosHautGaucheX, pPosBasDroiteY-pPosHautGaucheY, Slatch.ihm.getPanel());
+		g.drawImage(img, pPosHautGaucheX, pPosHautGaucheY, pPosBasDroiteX
+				- pPosHautGaucheX, pPosBasDroiteY - pPosHautGaucheY,
+				Slatch.ihm.getPanel());
 	}
 }
