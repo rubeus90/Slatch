2d6ad6329d4250f45d33ba530f1d3d08a24c54dd
martelandy764@gmail.com <martelandy764@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-06-10 07:51:27 +0000

    WAZAAAAAAAAAAAsvn updatesvn updatesvn update!

--- trunk/Moteur.java
+++ trunk/Moteur.java
@@ -135,10 +135,11 @@ class Moteur
             
             estMort(pVictime,uniteA);
             
-            if(getJoueur(uniteA).getFaction() == Faction.ROBOTS && uniteA.getType() == TypeUnite.KAMIKAZE){ //Si l'unite Attaquant est un Kamikaze
-                estMort(uniteA,pVictime);
-            }
-        }    
+        }
+        else if(getJoueur(uniteA).getFaction() == Faction.ROBOTS && uniteA.getType() == TypeUnite.KAMIKAZE) //Si l'unite Attaquant est un Kamikaze
+        {
+            estMort(uniteA,pVictime);
+        }
         else if(distance(uniteA, pVictime)==1 && pVictime.getAttaque().aTypePortee.getPorteeMin()==1) //sinon + si attaque au CAC, on riposte
         {
             //Pour les statistiques pour une attaque normal
