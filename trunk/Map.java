70936ddaeff25a906874f74a6483941744b4dbbf
t.patois@gmail.com <t.patois@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-06-10 13:31:08 +0000

    Nouvelle Map ... Ou PAs

--- trunk/Map.java
+++ trunk/Map.java
@@ -15,7 +15,8 @@ public enum Map{
     PARALLAXE("Parallaxe","Une map de taille moyenne","1V1","Parallaxe",40,15,2,false),
     SELETON("Seleton","Une grande map","3V1","seleton",30,30,4,false),
     NIVEAU1("Mission 1","La carte de la premiere mission de la campagne", "1V1","niveau1",25,12,2,true),
-    COULOIR("Couloir","Une map tout en long","1V1","couloir",30,13,2,false);
+    COULOIR("Couloir","Une map tout en long","1V1","couloir",30,13,2,false),
+    GAGNER("Gagner","Une Map de flemmarde","1V","gagner",10,10,2,false);
     
     private String aNom;
     private String aDescription;
