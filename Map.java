9dc7aea93be642ba239071703b788050cf7ba8c5
t.patois <7h1b0@users.noreply.github.com>
2013-06-11 09:21:55 +0000

    Ajout des maps desert

--- Map.java
+++ Map.java
@@ -6,17 +6,18 @@
  * @version (version number or date here)
  */
 public enum Map{
-    DOUBLEVAI("DoubleVai", "Une map en double V", "1V1","doublevai",32,18,2, false),
-    CHAMPS("Champs","Une map tres petite ou les combats sont intenses !","2V2 - 4","champs",12,12,4,false), 
-    HACHEMAP("HacheMap","Il ne faut pas etre une pelle pour gagner","1V1","hacheMap",20,20,2,false), 
-    JONATHAN("Jonathan","Map by Jonathan","2V2 - 4","jonathan",48,27,4,false),
-    MAPTEST("MapTest","Map de Test","","mapTest",32,18,2,false),
-    MAPTEST4("MapTest4","Map de Test","","mapTest4",32,18,4,false),
-    PARALLAXE("Parallaxe","Une map de taille moyenne","1V1","Parallaxe",40,15,2,false),
-    SELETON("Seleton","Une grande map","3V1","seleton",30,30,4,false),
-    NIVEAU1("Mission 1","La carte de la premiere mission de la campagne", "1V1","niveau1",25,12,2,true),
-    COULOIR("Couloir","Une map tout en long","1V1","couloir",30,13,2,false),
-    GAGNER("Gagner","Une Map de flemmarde","1V","gagner",10,10,2,false);
+    DOUBLEVAI("DoubleVai", "Une map en double V", "1V1","doublevai",32,18,2,false,false),
+    CHAMPS("Champs","Une map tres petite ou les combats sont intenses !","2V2 - 4","champs",12,12,4,false,false), 
+    HACHEMAP("HacheMap","Il ne faut pas etre une pelle pour gagner","1V1","hacheMap",20,20,2,false,false), 
+    JONATHAN("Jonathan","Map by Jonathan","2V2 - 4","jonathan",48,27,4,false,false),
+    MAPTEST("MapTest","Map de Test","","mapTest",32,18,2,false,false),
+    MAPTEST4("MapTest4","Map de Test","","mapTest4",32,18,4,false,false),
+    PARALLAXE("Parallaxe","Une map de taille moyenne","1V1","Parallaxe",40,15,2,false,false),
+    SELETON("Seleton","Une grande map","3V1","seleton",30,30,4,false,false),
+    NIVEAU1("Mission 1","La carte de la premiere mission de la campagne", "1V1","niveau1",25,12,2,false,true),
+    COULOIR("Couloir","Une map tout en long","1V1","couloir",30,13,2,false,false),
+    CHAMPSDESERT("Champs","Une map très petite dans le Desert","2V2 -4","champsdesert",12,12,4,true,false),
+    GAGNER("Gagner","Une Map de flemmarde","1V","gagner",10,10,2,false,false);
     
     private String aNom;
     private String aDescription;
@@ -26,11 +27,12 @@ public enum Map{
     private int aLargeur;
     private String aFichier;
     private boolean aLock;
+    private boolean aDesert;
     
     /**
      * Constructeur des Portees d'attaque
      */
-    Map(final String pNom,final String pDescription,final String pConseil, final String pFichier,final int pLongueur,final int pLargeur,final int pNbrJoueur,final boolean pLock){
+    Map(final String pNom,final String pDescription,final String pConseil, final String pFichier,final int pLongueur,final int pLargeur,final int pNbrJoueur,final boolean pDesert,final boolean pLock){
         aNom = pNom;
         aDescription = pDescription;
         aConseil = pConseil;
@@ -38,6 +40,7 @@ public enum Map{
         aNbrJoueur = pNbrJoueur;
         aLongueur=pLongueur;
         aLargeur=pLargeur;
+        aDesert=pDesert;
         aLock=pLock;
     }
     
@@ -103,6 +106,14 @@ public enum Map{
      */
     public boolean isVerrouille(){
         return aLock;
+    }
+    
+     /**
+     * Accesseur pour l'attribut aDescription
+     * @return aDescription
+     */
+    public boolean isDesert(){
+        return aDesert;
     }
     
     
