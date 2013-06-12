18d27856af4c092cfce81b21608747e3b8e4dbbf
martelandy764@gmail.com <martelandy764@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-06-12 14:35:50 +0000

    Ours is the fury

--- trunk/AIMaster.java
+++ trunk/AIMaster.java
@@ -1,63 +1,63 @@
-// import java.util.List;
-// import java.util.Iterator;
-// /**
-//  * Write a description of class AIMaster here.
-//  * 
-//  * @author (your name) 
-//  * @version (a version number or a date)
-//  */
-// public class AIMaster
-// {
-//     static int[][]iMap;
-//     static void joueTour(int joueur)
-//     {
-//         List<Unite> l = Slatch.partie.getJoueur(joueur).getListeUnite();
-//         GrandeIA.acheterUnite();
-//         
-//         iMap = new int[Slatch.partie.getLargeur()][Slatch.partie.getHauteur()];
-//         //remplirMap();
-//         Iterator<Unite> i = l.iterator();
-//         while(i.hasNext())
-//         {
-//             Unite u = i.next();
-//             if(u!=null){
-//                 if(!u.dejaAttaque()||!u.dejaDeplacee()){GrandeIA.elaborationObjectif(u);}
-//             }
-//             if(u.getPV()<=0){i.remove();}
-//             if(Slatch.partie.partieFinie){return;}
-//         }    
-//         Slatch.moteur.passeTour();
-//     }
-//     
-//     static void remplirMap()
-//     {
-//         for(int i=0; i<Slatch.partie.getLargeur(); i++)
-//         {
-//             for(int j=0; j<Slatch.partie.getHauteur(); j++)
-//             {
-//                 switch(Slatch.partie.getTerrain()[i][j].getType().getNom())
-//                 {
-//                     case "usine": if(Slatch.partie.getTerrain()[i][j].getJoueur()==Slatch.partie.getJoueurActuel()){iMap[i][j]+=30;}
-//                     else{iMap[i][j]+=10;} break;
-//                     case "batiment": iMap[i][j]+=40; break;
-//                     case "foret": iMap[i][j]+=20; break;
-//                     case "montagne": iMap[i][j]+=30; break;
-//                     case "qg": if(Slatch.partie.getTerrain()[i][j].getJoueur()==Slatch.partie.getJoueurActuel()){iMap[i][j]+=100;} else{iMap[i][j]+=40;}break;
-//                     default:
-//                 }
-//                 if(Slatch.partie.getTerrain()[i][j].getUnite()!=null)
-//                 {
-//                     spreadInfluence(Slatch.partie.getTerrain()[i][j].getUnite());
-//                 }
-//             }
-//         }
-//     }
-//     
-//     static void spreadInfluence(Unite unite)
-//     {
-//         for(int i=1; i<unite.getAttaque().aTypePortee.getPorteeMax(); i++)
-//         {
-//             
-//         }
-//     }
-// }
+import java.util.List;
+import java.util.Iterator;
+/**
+ * Write a description of class AIMaster here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AIMaster
+{
+    static int[][]iMap;
+    static void joueTour(int joueur)
+    {
+        List<Unite> l = Slatch.partie.getJoueur(joueur).getListeUnite();
+        OperationIA.acheterUnite();
+        
+        iMap = new int[Slatch.partie.getLargeur()][Slatch.partie.getHauteur()];
+        //remplirMap();
+        Iterator<Unite> i = l.iterator();
+        while(i.hasNext())
+        {
+            Unite u = i.next();
+            if(u!=null){
+                if(!u.dejaAttaque()||!u.dejaDeplacee()){GrandeIA.elaborationObjectif(u);}
+            }
+            if(u.getPV()<=0){i.remove();}
+            if(Slatch.partie.partieFinie){return;}
+        }    
+        Slatch.moteur.passeTour();
+    }
+    
+    static void remplirMap()
+    {
+        for(int i=0; i<Slatch.partie.getLargeur(); i++)
+        {
+            for(int j=0; j<Slatch.partie.getHauteur(); j++)
+            {
+                switch(Slatch.partie.getTerrain()[i][j].getType().getNom())
+                {
+                    case "usine": if(Slatch.partie.getTerrain()[i][j].getJoueur()==Slatch.partie.getJoueurActuel()){iMap[i][j]+=30;}
+                    else{iMap[i][j]+=10;} break;
+                    case "batiment": iMap[i][j]+=40; break;
+                    case "foret": iMap[i][j]+=20; break;
+                    case "montagne": iMap[i][j]+=30; break;
+                    case "qg": if(Slatch.partie.getTerrain()[i][j].getJoueur()==Slatch.partie.getJoueurActuel()){iMap[i][j]+=100;} else{iMap[i][j]+=40;}break;
+                    default:
+                }
+                if(Slatch.partie.getTerrain()[i][j].getUnite()!=null)
+                {
+                    spreadInfluence(Slatch.partie.getTerrain()[i][j].getUnite());
+                }
+            }
+        }
+    }
+    
+    static void spreadInfluence(Unite unite)
+    {
+        for(int i=1; i<unite.getAttaque().aTypePortee.getPorteeMax(); i++)
+        {
+            
+        }
+    }
+}
