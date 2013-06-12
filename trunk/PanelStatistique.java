826fb5009bf04083818ad16b7b6d766b83ec8d9d
nghongngoc90@gmail.com <nghongngoc90@gmail.com@75eed971-ea34-b657-6441-6fd9d8158e54>
2013-06-12 14:23:10 +0000

    Afficher tous les joueurs appartenant a l'equipe gagnant

--- trunk/PanelStatistique.java
+++ trunk/PanelStatistique.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
@@ -15,11 +17,7 @@ import javax.swing.JTextArea;
  * @author Ngoc
  *
  */
-public class PanelStatistique extends JPanel{
-	private JTextArea text1;
-	private JTextArea text2;
-	private JTextArea text3;
-	private JTextArea text4;
+public class PanelStatistique extends JPanel implements MouseListener{
 	private ArrayList<JTextArea> list;
 	
 	public PanelStatistique(){
@@ -41,8 +39,10 @@ public class PanelStatistique extends JPanel{
 			list.get(i).setForeground(Color.WHITE);
 			list.get(i).setEditable(false);
 			list.get(i).setFocusable(false);
+			list.get(i).addMouseListener(this);
 			afficheStat(i+1);
 		}
+		this.addMouseListener(this);
 		
 		this.repaint();
 	}
@@ -55,9 +55,19 @@ public class PanelStatistique extends JPanel{
 		Font font = Slatch.fonts.get("BlackOps").deriveFont(Font.PLAIN, this.getWidth()/20);
 		FontMetrics fm = getFontMetrics(font);
 		g.setFont(font);
-		String joueur = "Le joueur "+ Slatch.partie.getJoueurActuel() +" a gagné";
-		int longueur = fm.stringWidth(joueur);
-		g.drawString(joueur, this.getWidth()/2 - longueur/2, this.getHeight()/10);
+		String string = "Le joueur ";
+		
+		ArrayList<Joueur> equipe = Slatch.partie.getJoueur(Slatch.partie.getJoueurActuel()).getEquipe().getListeJoueur();
+		
+		for(Joueur joueur : equipe){
+			string += joueur.getNumJoueur() + ",";
+		}
+		
+		string = string.substring(0, string.length()-1);
+		string += " a gagné";
+		
+		int longueur = fm.stringWidth(string);
+		g.drawString(string, this.getWidth()/2 - longueur/2, this.getHeight()/10);
 		
 		Font font2 = Slatch.fonts.get("Visitor").deriveFont(Font.PLAIN, this.getWidth()/90);
 		for(int i=0; i< list.size(); i++){
@@ -88,4 +98,24 @@ public class PanelStatistique extends JPanel{
 					+ "Deplacement Total : " + Slatch.partie.ListeJoueur.get(i).getDeplacementTotal();
 		list.get(i-1).setText(stat);
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// Si la partie est finie
+        if(Slatch.partie.partieFinie && Slatch.partie.isCampagne()) {
+            Slatch.campagne.suite();
+        }
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
 }
