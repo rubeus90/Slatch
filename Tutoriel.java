228f8ae4b32c7bd31ee1b18cea8e0190efc7e15b
RollotQuentin <rollotquentin@gmail.com>
2013-06-17 13:44:52 +0000

    ajouts de fleches pour se déplacer dans les tuto

--- Tutoriel.java
+++ Tutoriel.java
@@ -32,8 +32,11 @@ public abstract class Tutoriel extends JPanel implements MouseListener
     }
     
     protected void afficheImageRedim (final String pURL, final int pPosHautGaucheX, final int pPosHautGaucheY,final int pPosBasDroiteX, final int pPosBasDroiteY, final Graphics g) {  
+        int tailleFleche = this.getWidth()/10;
         Image img = Slatch.aImages.get(pURL);
         g.drawImage(img, pPosHautGaucheX, pPosHautGaucheY, pPosBasDroiteX-pPosHautGaucheX, pPosBasDroiteY-pPosHautGaucheY, Slatch.ihm.getPanel());
+        g.drawImage(Slatch.aImages.get("flechegauche"), this.getWidth()/4,this.getHeight()/80,tailleFleche,tailleFleche, this);
+        g.drawImage(Slatch.aImages.get("flechedroite"), 3*this.getWidth()/4-tailleFleche,this.getHeight()/80,tailleFleche,tailleFleche, this);
     }
     
     @Override
@@ -45,8 +48,18 @@ public abstract class Tutoriel extends JPanel implements MouseListener
     
     @Override
 	public void mouseClicked(MouseEvent e) {
-	     aEtape++;   
-	     repaint();
+	    int tailleFleche = this.getWidth()/10;
+	    int pX= e.getX();
+	    int pY= e.getY();
+	    if(pX>this.getWidth()/4 && pX<this.getWidth()/4+tailleFleche && pY>this.getHeight()/80 && pY<this.getHeight()/80 +tailleFleche)
+	    {
+	        aEtape--; 
+	    }
+	    if(pX> 3*this.getWidth()/4-tailleFleche &&  pX<3*this.getWidth()/4 && pY>this.getHeight()/80 && pY<this.getHeight()/80+tailleFleche)
+	    {
+	        aEtape++; 
+	    }
+	    repaint();
     }
 
 	@Override
