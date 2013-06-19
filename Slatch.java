19bc8709f28a8d1068f2b3050265ab1719f163f9
RollotQuentin <rollotquentin@gmail.com>
2013-06-19 15:01:56 +0000

    yata

--- Slatch.java
+++ Slatch.java
@@ -302,6 +302,9 @@ public class Slatch {
                 image = ImageIO.read(getClass().getClassLoader().getResource("Images/campagne/background.png"));
                 aImages.put("background",image);
                 
+                image = ImageIO.read(getClass().getClassLoader().getResource("Images/campagne/campementjourplaine.png"));
+                aImages.put("campementjourplaine",image);
+                
                 image = ImageIO.read(getClass().getClassLoader().getResource("Images/campagne/barredialogue.png"));
                 aImages.put("barredialogue",image);
                 
